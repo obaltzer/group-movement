adf5c57f5cfc737bd8189d0a9e15d4cbd8b386fb
Oliver Baltzer <oliver@baltzer.net>
2007-04-04 20:10:55 +0000

    * changed resource limits for cluster operations

--- fim-app/afopt_max_rlimit.cpp
+++ fim-app/afopt_max_rlimit.cpp
@@ -26,8 +26,8 @@ void MineMaxPats();
 
 int main(int argc, char *argv[])
 {
-        struct rlimit cpu_limit = { 300, 300 };
-        struct rlimit mem_limit = { 268435456, 268435456};
+        struct rlimit cpu_limit = { 600, 600 };
+        struct rlimit mem_limit = { 419430400, 419430400};
         setrlimit(RLIMIT_CPU, &cpu_limit  );
         setrlimit(RLIMIT_AS, &mem_limit  );
         
